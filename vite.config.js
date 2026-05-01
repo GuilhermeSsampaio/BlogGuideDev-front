42f20b900168f1f5857d5f7693364c1a3bfd8141
GuilhermeSsampaio <guilhermessampaio20@gmail.com>
2026-05-01 14:36:33 -0400

    edit pwa

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,59 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: { enabled: true },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg,json,woff2}"],
+      },
+      manifest: {
+        name: "BlogGuide - o parceiro do dev",
+        short_name: "BlogGuide",
+        icons: [
+          {
+            src: "icons/icon-48x48.png",
+            sizes: "48x48",
+            type: "image/png",
+          },
+          {
+            src: "icons/icon-72x72.png",
+            sizes: "72x72",
+            type: "image/png",
+          },
+          {
+            src: "icons/icon-96x96.png",
+            sizes: "96x96",
+            type: "image/png",
+          },
+          {
+            src: "icons/icon-144x144.png",
+            sizes: "144x144",
+            type: "image/png",
+          },
+          {
+            src: "icons/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "icons/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+        start_url: "/",
+        display: "standalone",
+        background_color: "#fafafa",
+        theme_color: "#333ceb",
+      },
+    }),
+  ],
   base: "/",
   build: {
     outDir: "dist",
